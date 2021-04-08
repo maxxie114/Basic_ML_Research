d5a3677ca426b8e77d685a75326f7df6c03aa71e
maxxie114 <maxxie114@gmail.com>
2021-04-07 18:23:03 -0700

    add the log loss code seperately

--- classification/logistic_regression/scratch_implementation/exam.py
+++ classification/logistic_regression/scratch_implementation/exam.py
@@ -9,4 +9,26 @@ math_courses_taken = np.array([0,1,4,0,3,1,2,3,0,2,0,1,2,3,4,2,1,3,1,0]).reshape
 
 calculated_coefficients = np.array([[0.20678491]])
 
+zero_coefficients = np.array([0])
+
 intercept = np.array([-1.76125712])
+
+zero_intercept = np.array([0])
+
+def log_odds(features, coefficients,intercept):
+  return np.dot(features,coefficients) + intercept
+
+calculated_log_odds = log_odds(hours_studied,calculated_coefficients,intercept)
+
+calculated_log_odds_2 = log_odds(hours_studied,zero_coefficients,zero_intercept)
+
+def sigmoid(z):
+  denominator = 1 + np.exp(-z)
+  return 1/denominator
+
+probabilities = sigmoid(calculated_log_odds)
+
+probabilities_2 = sigmoid(calculated_log_odds_2)
+
+def log_loss(probabilities,actual_class):
+  return np.sum(-(1/actual_class.shape[0])*(actual_class*np.log(probabilities) + (1-actual_class)*np.log(1-probabilities)))
