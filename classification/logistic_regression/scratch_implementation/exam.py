e27556b1de0c0e9623ba75849ffc8f80c2c2f793
maxxie114 <maxxie114@gmail.com>
2021-04-08 01:02:05 -0700

    add fully functional gradient descent algorithm

--- classification/logistic_regression/scratch_implementation/exam.py
+++ classification/logistic_regression/scratch_implementation/exam.py
@@ -1,11 +1,9 @@
-# Code and data from Codecademy
+# Code and data from Codecademy with slight modification
 import numpy as np
 
 hours_studied = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]).reshape(20,1)
 
-passed_exam = np.array([0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1]).reshape(20,1)
-
-math_courses_taken = np.array([0,1,4,0,3,1,2,3,0,2,0,1,2,3,4,2,1,3,1,0]).reshape(20,1)
+passed_exam = np.array([0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1]).reshape(20,1)
 
 calculated_coefficients = np.array([[0.20678491]])
 
@@ -15,20 +13,3 @@ intercept = np.array([-1.76125712])
 
 zero_intercept = np.array([0])
 
-def log_odds(features, coefficients,intercept):
-  return np.dot(features,coefficients) + intercept
-
-calculated_log_odds = log_odds(hours_studied,calculated_coefficients,intercept)
-
-calculated_log_odds_2 = log_odds(hours_studied,zero_coefficients,zero_intercept)
-
-def sigmoid(z):
-  denominator = 1 + np.exp(-z)
-  return 1/denominator
-
-probabilities = sigmoid(calculated_log_odds)
-
-probabilities_2 = sigmoid(calculated_log_odds_2)
-
-def log_loss(probabilities,actual_class):
-  return np.sum(-(1/actual_class.shape[0])*(actual_class*np.log(probabilities) + (1-actual_class)*np.log(1-probabilities)))
--- classification/logistic_regression/scratch_implementation/logistic_regression.py
+++ classification/logistic_regression/scratch_implementation/logistic_regression.py
@@ -1,24 +1,73 @@
 # This is an implementation from scratch of the logistic regression
-# Algorithm, but it is untrained, and the gradient descent
-# algorithm and log loss for training still need to be 
-# implemented
-import numpy as np
-from exam import hours_studied, calculated_coefficients, intercept
 
-def log_odds(features, coefficients,intercept):
+# Algorithm of logistic regression
+# The main goal of logistic regression is to find the best coefficients, and intercept
+# to make the prediction accurate
+import numpy as np
+from exam import hours_studied, passed_exam
+
+# For some reason np array that start with an int will only be an int
+coefficients = np.array([[0.999999]])
+intercept = np.array([0.723534215])
+learning_rate = 0.008
+
+def log_odds(features, coefficients, intercept):
+  """Return the log odds for all the features"""
   return np.dot(features,coefficients) + intercept
 
 def sigmoid(z):
+    """Simulate the sigmoid function and return a result"""
     denominator = 1 + np.exp(-z)
     return 1/denominator
 
-# Create predict_class() function here
-def predict_class(features, coefficients, intercept, threshold):
+def log_loss(probabilities, actual_class, Xi):
+    """Return a summed log loss of all values in the feature dataset"""
+    return np.sum(-(1/actual_class.shape[0])*(actual_class*np.log(probabilities) + (1-actual_class)*np.log(1-probabilities))*Xi)
+
+def get_error_rate(predicted, actual):
+    """Return the average error rate"""
+    error = 0
+    for i in range(len(predicted)):
+        error += abs(predicted[i][0] - actual[i][0])
+    return error/len(predicted) 
+
+def train(features, labels):
+    n = len(features)
+    error_rate = 1
+    i = 0
+    print("Training Started!")
+    while error_rate > 0:
+        print(f"Iteration={i}")
+        # First get the probabilities
+        calculated_log_odds = log_odds(features, coefficients, intercept)
+        probabilities = sigmoid(calculated_log_odds)
+        # Calculate log loss
+        logloss = log_loss(probabilities, labels, features)
+        # Apply gradient descent algorithm
+        derivative_sigmoid = (1/n) * logloss
+        coefficients[0][0] = coefficients[0][0] - learning_rate*derivative_sigmoid
+        intercept[0] = intercept[0] - learning_rate*derivative_sigmoid
+        print(f"coefficient={coefficients},intercept={intercept}")
+        # Calculate error rate
+        result = np.where(probabilities >= 0.5, 1, 0)
+        old_error_rate = error_rate
+        error_rate = get_error_rate(result,labels)
+        print(f"[debug]error_rate:{error_rate}")
+        i += 1
+
+# Create predict_class() function
+def predict(features, threshold):
   calculated_log_odds = log_odds(features, coefficients, intercept)
   probabilities = sigmoid(calculated_log_odds)
   result = np.where(probabilities >= threshold, 1, 0)
   return result
 
+# First, train the model
+train(hours_studied, passed_exam)
+
 # Make final classifications on Codecademy University data here
-final_results = predict_class(hours_studied, calculated_coefficients, intercept, 0.5)
-print(final_results)
+final_results = predict(hours_studied, 0.5)
+predicted = final_results.tolist()
+print(f"predicted: {predicted}")
+actual = passed_exam.tolist()
+print(f"realY_val: {actual}")
